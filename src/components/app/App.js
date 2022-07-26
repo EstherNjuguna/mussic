66ed787976565d14da5fb9ebd48dccc6e5024f07
+ <+>
2022-07-26 13:37:50 +0300

    added routes

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import Navbar from '../navbar/Navbar';
 import './App.css';
-import {BrowserRouter as Router} from "react-router-dom"
+import Search from '../search/Search';
+import Dropdown from '../dropdown/Dropdown';
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar />
-      </Router>
+        <Routes>
+          <Route exact path="/" element={<Home/>}></Route>
+          <Route exact path="/search" element={<Search/>}></Route>
+          <Route exact path="/dropdown" element={<Dropdown/>}></Route>
+          </Routes>     
+     </Router>
     </div>
   );
   
