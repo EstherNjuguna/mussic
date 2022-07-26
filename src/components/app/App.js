cfab2f5e9c7ea4a6550a1ef5762dbd8a59531895
+ <+>
2022-07-26 20:57:21 +0300

    update Readme

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Navbar from '../navbar/Navbar';
 import './App.css';
 import Search from '../search/Search';
-import Dropdown from '../dropdown/Dropdown';
+import Music from '../music/Music';
 import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
 function App() {
   return (
@@ -10,9 +10,9 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route exact path="/" element={<Home/>}></Route>
-          <Route exact path="/search" element={<Search/>}></Route>
-          <Route exact path="/dropdown" element={<Dropdown/>}></Route>
+          <Route exact path="/" element={<home/>}></Route>
+          <Route exact path="/search" element={<search/>}></Route>
+          <Route exact path="/music" element={<music/>}></Route>
           </Routes>     
      </Router>
     </div>
