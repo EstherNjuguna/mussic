579ec488a6bf790eb08b0def21c302a2a35fe482
+ <+>
2022-07-26 21:11:25 +0300

    created login

---
+++ src/components/login/Login.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <form type="submit" className="login-form">
+      <h2 id="h2">Login</h2>
+      <input type="email" name="email" placeholder="Email" required />
+      <input type="password" name="password" placeholder="Password" required />
+      <button type="submit">Login</button>
+      <p>Don't have an account?<button id="account-btn">SignUp</button></p>
+    </form>
+  )
+}
+
+export default Login
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom"
 
 const Navbar = () => {
   return (
-    <nav className="navbar bg-light">Navbar
+    <nav className="navbar dark">Navbar
   <div className="container-fluid">
   <ul className="navbar-nav">
     <li className="nav-item">
@@ -14,6 +14,15 @@ const Navbar = () => {
 </li>
 <li className="nav-item">
 <Link className="nav-link"to="/dropdown">Dropdown</Link>
+</li>
+<li className="nav-item">
+<Link className="nav-link"to="/music">Music</Link>
+</li>
+<li className="nav-item">
+<Link className="nav-link"to="/login">Login</Link>
+</li>
+<li className="nav-item">
+<Link className="nav-link"to="/signup">Sinup</Link>
 </li>
   </ul>
   </div>
