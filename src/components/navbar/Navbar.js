c6c44d4380f79469541afd4e6ab1cfd387a425f3
+ <+>
2022-07-27 05:05:02 +0300

    styled Navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,17 +3,20 @@ import {Link} from "react-router-dom"
 
 const Navbar = () => {
   return (
-    <nav className="navbar dark">Navbar
-  <div className="container-fluid">
-  
-        <Link to="/">
-    Music search
-        </Link>
+    <div className="container">
+    <nav className="navbar navbar-expand-lg bg-primary">
+    <div className="container-fluid">
+    <Link to="/"> Music Search </Link>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" 
+                        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button> 
       
-  <ul className="navbar_links">
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
     <li className="nav-item">
-<Link className="nav-link"to="/">Home</Link>
-</li>
+   <Link to="/" className="nav-link active" aria-current="page">Home</Link>
+   </li>
 <li className="nav-item">
 <Link className="nav-link"to="/Search">Search</Link>
 </li>
@@ -26,10 +29,20 @@ const Navbar = () => {
 <li className="nav-item">
 <Link className="nav-link"to="/Signup">Signup</Link>
 </li>
+<li className="nav-item">
+   <Link to="/about" className="nav-link">About</Link>
+   </li>
   </ul>
+  <form className="d-flex" role="search">
+   <input className="form-control me-2" type="search" placeholder="Maroon 5" aria-label="Search"/>
+    <button className="btn btn-outline-success" type="submit">Search </button>
+            </form>
+  </div>
   </div>
   
   </nav>
+  </div>
+  
     )
 }
 
